21029623804018b2e2a751a26732b755d971bd53
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-07-23 20:22:45 -0300

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "ec2_keypair_name" {
+    default= "" 
+}
+
+variable "ec2_instance_type" {
+    default = "t2.micro" 
+}
+variable "ec2_image_id" {
+    default = "ami-05548f9cecf47b442"
+}
+
+variable "ec2_tags" {
+    default = "asbaba"
+  
+}
+variable "ec2_count" {
+    default = "1"
+}
