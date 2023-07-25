cb1b57e5ccd21c121572ed4fe14802358e6093c7
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-07-24 21:58:53 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
-  filename      = "func.js"
-  handler       = "func.js"
+  filename      = "func.zip"
+  handler       = "func.zip"
   runtime       = "nodejs18.x"
 }
