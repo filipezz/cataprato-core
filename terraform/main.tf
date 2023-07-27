734f1249d5154f58dc7de4a844192b2053e16b27
filipez <filipezmarron@hotmail.com>
2023-07-27 09:03:36 -0300

    refactor terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-resource "aws_lambda_function" "test_lambda" {
-  function_name     = "lambda_function_name"
-  role              = data.aws_iam_role.iam_for_lambda.arn
-  filename          = "./dist.zip"
-  handler           = "dist/lambda.js.handler"
-  runtime           = "nodejs18.x"
-  source_code_hash  =  filebase64sha256("./dist.zip")
+resource "aws_lambda_function" "this" {
+  function_name     =  var.function_name
+  role              =  data.aws_iam_role.iam_for_lambda.arn
+  filename          =  var.filename
+  handler           =  var.handler
+  runtime           =  var.runtime
+  source_code_hash  =  filebase64sha256(var.filename)
 }
