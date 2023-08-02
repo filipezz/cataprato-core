36ddba3d445ac769af4dc509d3ac2a389e2a34ab
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-01 23:09:11 -0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,22 +1,3 @@
-terraform {
- required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.9.0"
-    }
-  }
-  cloud {
-    organization = "Cataprato"
-
-    workspaces {
-      name = "cataprato-workspace"
-    }
-  }
-}
-provider "aws" {
-    region = "us-east-1"
-}
-
 module "lambda"{
 source        = "./terraform"
 
