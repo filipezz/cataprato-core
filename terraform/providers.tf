a3cd49395b17812e55715f47ed56ec54eaa7ced2
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-01 23:09:25 -0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,18 @@
-#terraform {
- #required_providers {
-  #  aws = {
-  #    source = "hashicorp/aws"
-  #    version = "5.9.0"
-  #  }
-  #}
-  #cloud {
-  #  organization = "Cataprato"
+terraform {
+ required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.9.0"
+    }
+  }
+  cloud {
+    organization = "Cataprato"
 
-   # workspaces {
-   #   name = "cataprato-workspace"
-    #}
-  #}
-#}
-#provider "aws" {
-#    region = "us-east-1"
-#}
+    workspaces {
+      name = "cataprato-workspace"
+    }
+  }
+}
+provider "aws" {
+    region = "us-east-1"
+}
