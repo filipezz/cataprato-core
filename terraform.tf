d7cac307e803c8fcae01a87625cfce3d6f0030d9
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-03 17:59:54 -0300

    Fix lambda workflow (#12)
    * Update release.yml
    
    * Update build.yml
    
    * Update terraform-plan.yml
    
    * Update terraform.tf
    
    * Update terraform-apply.yml
    
    * Update release.yml
    
    * Update terraform-apply.yml

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,26 @@
+terraform {
+ required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.9.0"
+    }
+  }
+  cloud {
+    organization = "Cataprato"
+
+    workspaces {
+      name = "cataprato-workspace"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
-
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
 }
+
 
 data "aws_caller_identity" "current" {}
 
